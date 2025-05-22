df652cd4253a731bfa0d912916fb7cb3d96ed570
Cwleoniszyh <2023213267@bupt.cn>
2025-05-22 17:09:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import yaml
 
 class Config:
     def __init__(self):
-        with open("E:\py code\RunningMan\iOSRealRun-cli-18-main\config.yaml", 'r') as f:
+        with open(config.yaml, 'r') as f:
             config = yaml.safe_load(f)
         for i in config:
             setattr(self, i, config[i])
